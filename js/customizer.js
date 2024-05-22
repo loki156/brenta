d72ddf99d75b2a05cb004dc4bebb56a59d9af417
loki156 <85053221+loki156@users.noreply.github.com>
2024-05-22 11:58:12 +0200

    Update customizer.js

--- js/customizer.js
+++ js/customizer.js
@@ -7,29 +7,33 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-( function( $ ) {
-    wp.customize('page_custom_classes', function(value) {
-        value.bind(function(newval) {
-            $('#primary').attr('class', 'site-main inner ' + newval);
-        });
-    });
-    // Ensure DOM is ready
-    $(document).ready(function() {
-        // Modify the label to include a span for dynamic width display
-        var label = $('#customize-control-logo_width_control .customize-control-title');
-        if (label.length) {
-            label.html('Logo Width: <span class="width-value">100px</span>'); // Initialize with default value
-        }
+jQuery(document).ready(function($) {
+    // Modify the label to include a span for dynamic width display
+    var label = $('#customize-control-logo_width_control .customize-control-title');
+    if (label.length) {
+        label.html('Logo Width: <span class="width-value">100px</span>'); // Initialize with default value
+    }
 
-        // Update the span when the control value changes
-        wp.customize('logo_width', function(value) {
-            value.bind(function(newVal) {
-                $('.width-value').text(newVal + 'px'); // Update the label text dynamically
-                $('.custom-logo').css('width', newVal + 'px'); // Also update the actual logo width
-            });
+    // Update the span when the control value changes
+    wp.customize('logo_width', function(value) {
+        value.bind(function(newVal) {
+            $('.width-value').text(newVal + 'px'); // Update the label text dynamically
+            $('.custom-logo').css('width', newVal + 'px'); // Also update the actual logo width
         });
-        
     });
-    
-}( jQuery ) );
+
+    // Main Footer Top Padding
+    wp.customize('main_footer_top_padding', function(value) {
+        value.bind(function(newval) {
+            $('.hs-footer .gutter').css('padding-top', newval + 'px');
+        });
+    });
+
+    // Main Footer Bottom Padding
+    wp.customize('main_footer_bottom_padding', function(value) {
+        value.bind(function(newval) {
+            $('.hs-footer .gutter').css('padding-bottom', newval + 'px');
+        });
+    });
+});
 
